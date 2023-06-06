2bc2c89cfbab32dadb48075be051a1dd784b04a6
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-06-06 09:46:31 -0500

    Creación de instancias y se establece el valor de pi

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String args[]){
+
+        //Instancias
+        figuras3D esfera = new figuras3D(6.3f,3.14);
+        figuras3D cilindro = new figuras3D(9.3f, 8.3f, 3.14);
+        figuras3D cubo = new figuras3D(7.3f);
+        figuras3D piramide = new figuras3D(4.2f, 7.4f);
+
+
+        double pi= esfera.getPi();
+        esfera.setPi(pi);
+        cilindro.setPi(pi);
+
     }
+
 }
